a4135320a225b31d8a29580589b4589539676ce3
saddamtitanio <88148304+saddamtitanio@users.noreply.github.com>
2024-01-03 11:17:12 +0700

    Add files via upload

---
+++ TugasModul1/pattern_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(void) {
+    int n;
+    int count = 0; 
+
+    do {
+        std::cout << "N = ";
+        std::cin >> n;
+
+    } while (n < 0);
+
+    
+    for (int i = 0; i < n; i++) {
+        int column = i <= (n / 2) ? (n / 2) + 1 - i : i - (n / 2) + 1;
+        int spaces = (i <= n / 2) ? i : n / 2;
+        
+        for (int k = 0; k < spaces; k++) {
+            std::cout << " ";
+        }
+
+        for (int j = 0; j < column; j++, count++) {
+            std::cout << count % 10;
+        }
+        std::cout << '\n';
+    }
+    return 0;
+}
